91306220398cfc09ca595d11f8bf365f4f66f441
Dominic Sherman <dominic.sherman98@gmail.com>
2022-03-31 10:42:14 -0700

    Specify everything

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,21 @@
+/* eslint-disable no-undef */
 // https://typescript-eslint.io/docs/linting/
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json'],
-  },
   plugins: ['@typescript-eslint', 'jest'],
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
     'plugin:jest/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: ['plugin:@typescript-eslint/recommended'],
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json'],
+      }
+    }
+  ]
 }
---
+++ versioning/tests/main.test.ts
@@ -0,0 +1,5 @@
+describe('main', () => {
+  it('should run tests', () => {
+    expect(true).toBe(true);
+  });
+});
