dc1b2c469eca48673143438223af4d5811a07e1f
Dominic Sherman <dominic.sherman98@gmail.com>
2022-04-04 13:20:33 -0700

    Clean up github to use octokit

--- tagging/src/utils/github.ts
+++ tagging/src/utils/github.ts
@@ -1,15 +1,6 @@
-import fetch from 'node-fetch';
-import { DEFAULT_APP_VERSION } from '../constants';
+import * as github from '@actions/github';
 
-interface CommitItem {
-  commit: {
-    message: string;
-  };
-  html_url: string;
-  author: {
-    login: string;
-  };
-}
+import { DEFAULT_APP_VERSION } from '../constants';
 
 interface CreateGithubTagProps {
   githubAuthToken: string;
@@ -20,70 +11,33 @@ interface CreateGithubTagProps {
 
 export const createGithubTag = async ({
   githubAuthToken,
-  branchToTag,
   currentTag,
   newTag,
 }: CreateGithubTagProps): Promise<void> => {
-  const headers = getHeaders(githubAuthToken);
+  const octokit = github.getOctokit(githubAuthToken);
 
-  // get commits to HEAD since most recent tag
-  const commitSinceTagUrl = `${process.env.GITHUB_API_URL}/repos/${process.env.GITHUB_REPOSITORY}/compare/${currentTag}...HEAD`;
-  const commitsSinceTagResponse = await fetch(commitSinceTagUrl, { headers });
-  const commitSinceTagData = await commitsSinceTagResponse.json();
-  const commits = commitSinceTagData.commits || [];
-
-  // generate list of commit messages for release body
-  const commitMessages = commits.map(
-    (item: CommitItem) => `* ${item.commit.message} ${item.html_url} - @${item.author.login}`
-  );
-
-  const [owner, repo] = (process.env.GITHUB_REPOSITORY || '').split('/');
-
-  // generate body for release
-  const body = `${commitMessages.join('\n ')}`;
-
-  // create release
-  await fetch(`${process.env.GITHUB_API_URL}/repos/${process.env.GITHUB_REPOSITORY}/releases`, {
-    body: JSON.stringify({
-      owner,
-      repo,
-      tag_name: newTag,
-      target_commitish: branchToTag,
-      name: newTag,
-      body,
-    }),
-    method: 'POST',
-    headers,
+  const { data: releaseNotes } = await octokit.rest.repos.generateReleaseNotes({
+    ...github.context.repo,
+    tag_name: newTag,
+    previous_tag_name: currentTag,
+  });
+  await octokit.rest.repos.createRelease({
+    ...github.context.repo,
+    ...releaseNotes,
+    target_commitish: github.context.sha,
+    tag_name: newTag,
   });
 };
 
 export const getMostRecentGithubTag = async (githubAuthToken: string): Promise<string> => {
-  // get existing tags
-  const tagsResponse = await fetch(
-    `${process.env.GITHUB_API_URL}/repos/${process.env.GITHUB_REPOSITORY}/tags`,
-    {
-      headers: getHeaders(githubAuthToken),
-    }
-  );
-  const tagsData = await tagsResponse.json();
+  const octokit = github.getOctokit(githubAuthToken);
+  const tagsResponse = await octokit.rest.repos.listTags({
+    ...github.context.repo,
+    per_page: 10,
+  });
+  const tags = tagsResponse?.data || [];
 
-  // default if the repository doesn't have any tags created yet
-  let mostRecentTag = `v${DEFAULT_APP_VERSION}-1`;
-
-  if (tagsData && Array.isArray(tagsData) && tagsData[0] && tagsData[0].name) {
-    // specify most recent tag if there are existing tags in the repo
-    mostRecentTag = tagsData[0].name;
-  }
+  const mostRecentTag = tags[0]?.name || `v${DEFAULT_APP_VERSION}-1`;
 
   return mostRecentTag;
-};
-
-const getHeaders = (token: string) => {
-  if (!token) {
-    throw new Error('Github auth token not specified');
-  }
-  return {
-    Accept: 'application/vnd.github.v3+json',
-    Authorization: `Bearer ${token}`,
-  };
 };
